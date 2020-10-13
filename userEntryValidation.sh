3dccfa186cd20bffe36c15cdec29033b30a2c55d
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 04:54:29 +0530

    last name validation

--- userEntryValidation.sh
+++ userEntryValidation.sh
@@ -11,3 +11,13 @@ then
 else
 	echo "invalid"
 fi
+
+#uc2 last name validation
+read -p "enter lastname:" last_name
+lastname_Pattern="^[A-Z][a-z]{2,}$"
+if [[ $last_name =~ $lastname_Pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
