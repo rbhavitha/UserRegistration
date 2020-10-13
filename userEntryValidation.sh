0221380a50e03d09eab002572542177987dcf1e5
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 04:49:54 +0530

    first name validation

--- userEntryValidation.sh
+++ userEntryValidation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Ensure all validations are in place during user entry"
+
+#uc1 first name validation
+read -p "enter firstname:" first_name
+firstname_Pattern="^[A-Z][a-z]{2,}$"
+if [[ $first_name =~ $firstname_Pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
