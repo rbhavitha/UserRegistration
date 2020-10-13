93f11dce6a2f7452586d04541211341d30d3049d
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 04:59:24 +0530

    email validation

--- userEntryValidation.sh
+++ userEntryValidation.sh
@@ -21,3 +21,13 @@ then
 else
         echo "invalid"
 fi
+
+#uc3 email validation
+read -p "enter email:" email
+email_Pattern="^[0-9a-zA-Z]+([_.+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?"
+if [[ $email =~ $email_Pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
