4d55386cb8804335dd34a0e6b54ca4c983df1927
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 05:05:30 +0530

    mobile number validation

--- userEntryValidation.sh
+++ userEntryValidation.sh
@@ -31,3 +31,13 @@ then
 else
         echo "invalid"
 fi
+
+#uc4 mobile number validation
+read -p "enter mobile_num:" mobile_num
+mobilenum_Pattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+if [[ $mobile_num =~ $mobilenum_Pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
